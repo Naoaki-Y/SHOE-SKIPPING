6c4f9f466418e1959f1290e00bc1488e4e367d84
Naoaki-Y <diberidarshi@yahoo.co.jp>
2017-05-04 08:04:24 +0900

    ・靴との通信部分追加 ・靴のセンサー情報取得部分追加

---
+++ BluetoothCOM/BluetoothCOM/SettingFrm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace BluetoothCOM
+{
+    partial class SettingFrm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLog.Location = new System.Drawing.Point(12, 124);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.Size = new System.Drawing.Size(464, 132);
+            this.textBoxLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Location = new System.Drawing.Point(77, 54);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(297, 25);
+            this.textBoxPath.TabIndex = 3;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPath.Location = new System.Drawing.Point(378, 47);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(98, 38);
+            this.btnPath.TabIndex = 4;
+            this.btnPath.Text = "open";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "COM";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10"});
+            this.comboBoxPort.Location = new System.Drawing.Point(77, 12);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(169, 26);
+            this.comboBoxPort.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 263);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(464, 42);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "BluetoothCOM";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // SettingFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 315);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "SettingFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BluetoothCOM";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SettingFrm_FormClosed);
+            this.Load += new System.EventHandler(this.SettingFrm_Load);
+            this.ResizeEnd += new System.EventHandler(this.SettingFrm_ResizeEnd);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ BluetoothCOM/BluetoothCOM/SettingFrm.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluetoothCOM
+{
+    public partial class SettingFrm : Form
+    {
+        public int BaudRate { get; set; }
+        public bool IsRunning { get; private set; }
+        private string recvMessage;
+
+        SerialPort serialPort;
+        public SettingFrm()
+        {
+            InitializeComponent();
+            this.BaudRate = 9600;
+            comboBoxPort.SelectedIndex = 0;
+        }
+
+        private bool OpenCom()
+        {
+            /*
+            try {
+                this.serialPort = new SerialPort(this.comboBoxPort.SelectedItem.ToString(), BaudRate, Parity.None, 8, StopBits.One);
+                this.serialPort.DataReceived += SerialPort_DataReceived;
+                this.serialPort.ErrorReceived += SerialPort_ErrorReceived;
+                this.serialPort.Open();
+                this.IsRunning = true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                serialPort = null;
+                return false;
+            }
+            */
+
+            return true;
+        }
+
+        private void SerialPort_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            if (IsRunning)
+            {
+                btnStart.PerformClick();
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string recv = this.serialPort.ReadLine();
+
+            if(recv == recvMessage)
+            {
+                return;
+            }
+
+
+            try {
+                using (StreamWriter write = new StreamWriter(Path.Combine()))
+                {
+                    write.WriteLine(recvMessage);
+                }
+                recvMessage = recv;
+
+                this.Invoke(new MethodInvoker(() => textBoxLog.Text += String.Format("{0} {1}{2}", DateTime.Now.ToString("HH:mm.ss.fff"), recvMessage, Environment.NewLine)));
+            }
+            catch
+            {
+            }
+
+        }
+
+
+        private bool CloseCom()
+        {
+            if(this.serialPort == null || !this.serialPort.IsOpen){
+                return true;
+            }
+
+            /*
+            try {
+                serialPort.Close();
+            }
+            catch(IOException e)
+            {
+                MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            */
+            return true;
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.SelectedPath = textBoxPath.Text;
+
+            if(folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            textBoxPath.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void SettingFrm_Load(object sender, EventArgs e)
+        {
+            this.textBoxPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            notifyIcon.Visible = true;
+            notifyIcon.Icon = SystemIcons.Application;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (IsRunning)
+            {
+                if (CloseCom())
+                {
+                    btnPath.Enabled = true;
+                    textBoxPath.Enabled = true;
+                    comboBoxPort.Enabled = true;
+                    btnStart.Text = "START";
+                }
+            }
+            else
+            {
+
+                if(!Directory.Exists(textBoxPath.Text))
+                {
+                    MessageBox.Show("Not Found. Directory", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (OpenCom())
+                {
+                    btnPath.Enabled = false;
+                    textBoxPath.Enabled = false;
+                    comboBoxPort.Enabled = false;
+                    btnStart.Text = "STOP";
+                }
+
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void SettingFrm_ResizeEnd(object sender, EventArgs e)
+        {
+            if(WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void SettingFrm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (IsRunning)
+            {
+                CloseCom();
+            }
+        }
+    }
+}
